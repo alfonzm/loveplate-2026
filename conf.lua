6cbc04648ae5e60d59ba45c4b332a44ad3d350be
AlfonzM <m.alfonz@gmail.com>
2026-09-07 20:44:20 +0800

    tilesize

--- conf.lua
+++ conf.lua
@@ -1,15 +1,17 @@
-G = {
-  -- set to false on release/build
-  dev = true,
+local tileSize = 16
 
-  title = 'loveplate-2026',
-  scale = 3,
-  -- tile_size = 16,
-  width = 16 * 36,
-  height = 16 * 20,
-  fullscreen = false,
-  debug = false,
-  debugCollider = false,
+G = {
+    -- set to false on release/build
+    dev = true,
+
+    title = 'loveplate-2026',
+    scale = 4,
+    tileSize = tileSize,
+    width = tileSize * 10,
+    height = tileSize * 9,
+    fullscreen = false,
+    debug = false,
+    debugCollider = false,
 }
 
 -- Playable area (hard-coded 3× logical viewport for now).
@@ -17,9 +19,9 @@ G.worldWidth = G.width * 3
 G.worldHeight = G.height * 3
 
 function love.conf(t)
-  t.identity = "loveplate-2026"
-  t.window.title = G.title
-  t.window.resizable = false
-  t.window.width = G.width * G.scale
-  t.window.height = G.height * G.scale
+    t.identity = "loveplate-2026"
+    t.window.title = G.title
+    t.window.resizable = false
+    t.window.width = G.width * G.scale
+    t.window.height = G.height * G.scale
 end
