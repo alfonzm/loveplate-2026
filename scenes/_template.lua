0f96dd33ac23231714ea52d518bd6ab490c4822e
AlfonzM <m.alfonz@gmail.com>
2026-09-07 20:54:06 +0800

    improve scene template

--- scenes/_template.lua
+++ scenes/_template.lua
@@ -1,10 +1,21 @@
--- TEMPLATE: copy to myScene.lua and rename `myScene` everywhere below.
+-- TEMPLATE: copy to myScene.lua and rename `_renameMe` everywhere below.
 local Scene = require "lib.alphonsus.scene"
 
 local _renameMe = Scene:extend()
 
+function _renameMe:new()
+    _renameMe.super.new(self)
+    -- self.bgColor = { 0.06, 0.06, 0.06, 1 }
+    return self
+end
+
 function _renameMe:enter()
     _renameMe.super.enter(self)
+    -- self:add(SomeEntity())
+end
+
+function _renameMe:onLeave()
+    _renameMe.super.onLeave(self)
 end
 
 function _renameMe:stateUpdate(dt)
