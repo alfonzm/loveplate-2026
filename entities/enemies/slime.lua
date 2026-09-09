157ed83328445840ec6913e26dfa7de5bf33fd54
AlfonzM <m.alfonz@gmail.com>
2026-09-09 21:32:15 +0800

    add basic animation

---
+++ entities/enemies/slime.lua
@@ -0,0 +1,29 @@
+local anim8 = require "lib.anim8"
+
+local GameObject = require "lib.alphonsus.gameObject"
+
+local Slime = GameObject:extend()
+
+function Slime:new(opts)
+    Slime.super.new(self)
+    opts = opts or {}
+    self.name = "myEntity"
+    self.x = opts.x or 0
+    self.y = opts.y or 0
+
+    self.spritesheet = love.graphics.newImage("assets/img/slime_sheet.png")
+    local g = anim8.newGrid(16, 16, self.spritesheet:getWidth(), self.spritesheet:getHeight())
+    self.animation = anim8.newAnimation(g('1-3', 1, '2-2', 1), 0.15)
+
+    return self
+end
+
+function Slime:update(dt)
+    self.animation:update(dt)
+end
+
+function Slime:draw()
+    self.animation:draw(self.spritesheet, self.x, self.y)
+end
+
+return Slime
--- scenes/tilemap.lua
+++ scenes/tilemap.lua
@@ -1,6 +1,7 @@
 local Scene = require "lib.alphonsus.scene"
 local Tilemap = require "entities.tilemap"
 local Link = require "entities.link"
+local Slime = require "entities.enemies.slime"
 local cameraFollow = require "lib.alphonsus.cameraFollow"
 
 local tilemap = Scene:extend()
@@ -13,6 +14,9 @@ function tilemap:enter()
     local link = self:add(Link({ x = 16 + 8, y = 16 + 8 }))
 
     cameraFollow.setTarget(link, {smoothTime = 0})
+
+    -- slime
+    self:add(Slime({ x = 16 * 5, y = 16 * 5 }))
 end
 
 return tilemap
